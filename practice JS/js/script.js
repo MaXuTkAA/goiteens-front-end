2b58e7de86bf07a3fa8622cbd976db6df735ff0c
unknown <revizor615@gmail.com>
2021-01-23 20:51:25 +0300

    JS

---
+++ practice JS/js/script.js
@@ -0,0 +1,46 @@
+// console.log("test")
+// -------------------- 2 завдвння --------------------
+const a = 20;
+console.log(a); //20
+
+const age = 12;
+console.log(12); //12
+
+const number = 1.97;
+console.log(1.97); //1.97
+
+const c = 112;
+console.log(112); //112
+
+const num1 = 15.68;
+console.log(15.68); //15.68
+
+
+let age1;
+console.log(age1);
+
+let final;
+console.log(final);
+
+let long;
+console.log(long);
+
+let num2;
+console.log(num2);
+
+let d;
+console.log(d);
+
+
+
+
+
+// -------------------- 1 завдвння --------------------
+console.log(typeof age);
+
+const name = 'Maks';
+console.log(typeof name);
+
+const hasChildren = true;
+console.log(typeof hasChildren);
+
