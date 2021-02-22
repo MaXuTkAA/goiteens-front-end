edd74a94edc896b6323e72db782cc6acbcd0249b
unknown <revizor615@gmail.com>
2021-02-22 20:45:50 +0200

    JS3

--- practice JS/hw_2_js/js/script.js
+++ practice JS/hw_2_js/js/script.js
@@ -1,2 +1,72 @@
 "use strict"
-console.log("test")
+// console.log("test")
+
+//         1 завдання       //
+const value = prompt('What is your name?');
+console.log(value);
+
+
+//         3 завдання       //
+const a = 6;
+const b = 2;
+console.log(a * b); // 12
+
+
+//         4 завдання       //
+let a1 = 4 + 2;
+let a2 = 4 - 2;
+let a3 = 4 * 2;
+let a4 = 4 / 2;
+
+alert(a1);
+alert(a2);
+alert(a3);
+alert(a4);
+
+
+//         5 завдання       //
+let height = 23;
+let width = 10; 
+let s = 23 * 10;
+console.log(s);
+
+//         6 завдання       //
+let a6 = 5 % 3;
+let a7 = 3 % 5;
+let a8 = 5 + '3';
+let a9 = '5' - 3;
+let a10 = 75 + 'кг';
+
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+alert(a10);
+
+//         8 завдання       //
+const n = 3;
+const m = 4;
+let k = 0;
+
+k = (n ** 2) + Math.pow(m , 2);
+k = Math.sqrt(k);
+console.log('k = ', k)
+
+
+//         7 завдання       //
+let v = 0;
+const dc = prompt('Введіть діаметр', 10);
+console.log(dc);
+
+const h = 35;
+const r = dc/2;
+
+v = Math.PI * (r ** 2) * h;
+
+console.log('v = ', v);
+
+
+//         2 завдання       //
+let MyAge = Number(prompt("Введіть ваш вік"));
+let FriendAge = Number(prompt("Введіть вік вашого друга"));
+alert(("Ваш спільний вік:") + (MyAge + FriendAge));
---
+++ practice JS/hw_3_js/js/script.js
@@ -0,0 +1,68 @@
+"use strict"
+// console.log("test")
+
+//         4 завдання        //
+console.log(Math.random() * (19 - 3) + 3);
+
+
+//         3 завдання        //
+console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+
+
+//         3 завдання        //
+const name = 'Viktor';
+const fullName =
+  'My ' + 'name ' + 'is ' + name + '.';
+  console.log(fullName);
+
+
+  //         1 завдання        //
+  
+  let k1 = '5px';
+  let k2 = '12djd334';
+  let k3 = '12.45asdwe12';
+  let k4 = 'qwqweeewq';
+  
+//           1 частина         //
+
+  console.log(Number.parseInt(k1));
+  
+  console.log(Number.parseInt(k2));
+  
+  console.log(Number.parseInt(k3));
+  
+  console.log(Number.parseInt(k4));
+
+//           2 частина         //
+console.log(Number.parseFloat(k1));
+
+console.log(Number.parseFloat(k2));
+
+console.log(Number.parseFloat(k3));
+
+console.log(Number.parseFloat(k4));
+
+  //         5 завдання        //
+  let result;
+
+result = 5 + '5';
+console.log(result); // '55'
+console.log(typeof result); // string
+
+
+  //         6 завдання        //
+  const email  = 'revizor615@gmail.com';
+  console.log(email.length);
+  //console.log('revizor615@gmail.com'.length);
+
+
+  //         8 завдання        //
+ const userName = 'Олександрa';
+ const payment = '200';
+ const a = ' Дякуємо, Олександро!';
+ const b = ' До сплати "payment" гривень';
+ alert(`Змінна userName = ${a}, змінна payment = ${b}`);
+
+
+
